4e603fa69d21eb01ee17469f7dd5d32c3869cd76
Aragrez <vicentegrez@gmail.com>
2026-03-12 23:35:11 -0300

    Agregar import Request

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Request
 from pydantic import BaseModel
 from typing import List, Optional
 from datetime import date
